564df93a77bf55c9f763015647ec61fa643a2860
RemoCharles <61230079+RemoCharles@users.noreply.github.com>
2020-05-29 13:14:56 +0200

    Added a comment

--- restart_task.bat
+++ restart_task.bat
@@ -2,6 +2,6 @@
 :start
 :: TODO - add the path to your app
 start /w "" "C:\path\to\your\app.exe"
-:: TODO - time to start after the app stops
+:: TODO - time to start after the app stops (1800 seconds = 30 minutes)
 TIMEOUT /T 1800
 goto start
